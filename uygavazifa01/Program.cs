d36b1731a2b42c6e09ceca66d15adcb20f5fad37
Rus19tam98 <128899293+Rus19tam98@users.noreply.github.com>
2023-03-30 22:02:22 +0500

    10 likdan 2 likka o'tish

---
+++ uygavazifa01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace uygavazifa01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" son kiriting => ");
+            int a = int.Parse(Console.ReadLine());
+            string str = "";
+            while (a >= 1)
+            {
+                str = (a % 2) + str;
+                a = a / 2;
+            }
+            Console.Write($"natija => {str}");
+            Console.WriteLine();
+        }
+    }
+}
